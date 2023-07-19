6c71f933e443853558ff1331221ab5ff2e8895ce
EnderMo23 <fischer.moritz09@gmail.com>
2023-07-19 17:38:40 +0200

    Huso

--- src/main/kotlin/challanges/AllItems.kt
+++ src/main/kotlin/challanges/AllItems.kt
@@ -2,7 +2,12 @@ package me.sirsam.challenges.challanges
 
 import me.sirsam.challenges.ChallengeTimer
 import me.sirsam.challenges.helpers.ChallengeStatus
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarStyle
+import org.bukkit.boss.BossBar
 import org.bukkit.entity.Player
 
 class AllItems {
@@ -13,5 +18,17 @@ class AllItems {
         loc.y += 2.5
         //val display =
         //player.world.spawn(loc, ItemDisplay)
+    }
+
+    fun getBossbar(): BossBar {
+        val item = Material.values().random()
+        val bossBar = Bukkit.createBossBar(item.name, BarColor.RED, BarStyle.SOLID)
+
+        return bossBar
+    }
+
+    fun showBossbar(player: Player) {
+        //player.showBossBar(getBossbar())
+
     }
 }
--- src/main/kotlin/listeners/OnJoin.kt
+++ src/main/kotlin/listeners/OnJoin.kt
@@ -1,6 +1,7 @@
 package me.sirsam.challenges.listeners
 
 import me.sirsam.challenges.helpers.Scoreboard
+import org.bukkit.boss.BossBar
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
