1f5d4cf68f5bde9cec831a078e20bb1e8be10f7f
EnderMo23 <fischer.moritz09@gmail.com>
2023-07-19 19:16:42 +0200

    Boss bar

--- src/main/kotlin/challanges/AllItems.kt
+++ src/main/kotlin/challanges/AllItems.kt
@@ -2,6 +2,8 @@ package me.sirsam.challenges.challanges
 
 import me.sirsam.challenges.ChallengeTimer
 import me.sirsam.challenges.helpers.ChallengeStatus
+import net.kyori.adventure.bossbar.BossBar
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.BlockDisplay
@@ -30,5 +32,15 @@ class AllItems {
 
     fun getItem(player: Player): Material? {
         return item[player]
+    }
+
+    fun getBossbar(): BossBar {
+        val item = Material.values().random()
+        val bossbar = BossBar.bossBar(Component.text(item.name), 100f, BossBar.Color.PURPLE, BossBar.Overlay.PROGRESS)
+        return bossbar
+    }
+
+    fun sendBossbar(player: Player) {
+        player.showBossBar(getBossbar())
     }
 }
