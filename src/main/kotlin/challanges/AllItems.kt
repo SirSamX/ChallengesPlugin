6a0c8fa9b9b3dc162ee370c97c941b790f16ad2b
SirSam <samuelheil@web.de>
2023-07-28 23:15:05 +0200

    Updated

--- src/main/kotlin/challanges/AllItems.kt
+++ src/main/kotlin/challanges/AllItems.kt
@@ -1,94 +1,62 @@
 package me.sirsam.challenges.challanges
 
 import me.sirsam.challenges.ChallengeTimer
+import me.sirsam.challenges.Main
 import me.sirsam.challenges.helpers.Challenge
 import me.sirsam.challenges.helpers.ChallengeStatus
+import me.sirsam.challenges.helpers.Utilities
 import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.Material
-import org.bukkit.entity.ArmorStand
-import org.bukkit.entity.BlockDisplay
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.ItemDisplay
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
-import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryPickupItemEvent
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.MainHand
-import org.bukkit.scheduler.BukkitRunnable
-import org.eclipse.sisu.launch.Main
-import javax.swing.event.ChangeEvent
 
-class AllItems : Challenge() {
-    private var item: MutableMap<Player, Material> = mutableMapOf()
+class AllItems : Challenge, Listener {
+    private var items: MutableMap<Player, Material> = mutableMapOf()
 
     fun newItem(player: Player) {
-        item[player] = Material.values().random()
+        items[player] = Material.values().random()
 
-        if (ChallengeTimer.timer.getStatus() != ChallengeStatus.ACTIVE || item[player] == null) return
-        val loc = player.location
-        val armorStand = loc.world.spawnEntity(loc, EntityType.ARMOR_STAND) as ArmorStand
-        armorStand.isInvisible = true
-        armorStand.isSmall = true
-        armorStand.isInvulnerable = true
-        armorStand.setItem(EquipmentSlot.HEAD, ItemStack(item[player]!!))
-        player.addPassenger(armorStand)
+        sendBossbar(player)
+        player.playerListName(Component.text(player.name).append(Component.text(" ")).append(ItemStack(items[player]!!).displayName().color(NamedTextColor.LIGHT_PURPLE)))
+        if (ChallengeTimer.timer.getStatus() != ChallengeStatus.ACTIVE || items[player] == null) return
 
-        /*if (item[player]!!.isBlock) {
-            player.sendMessage("BLOCK")
-            val display = player.world.spawn(loc, BlockDisplay::class.java)
-            display.block = Bukkit.createBlockData(item[player]!!)
-            object : BukkitRunnable() {
-                override fun run() {
-                    val location = player.location
-                    loc.y += 2.5
-                    display.teleport(location)
-                }
-            }.runTaskTimer(me.sirsam.challenges.Main.getPlugin(), 0, 1L)
-        } else {
-            player.sendMessage("ITEM")
-            val display = player.world.spawn(loc, ItemDisplay::class.java)
-            display.itemStack = ItemStack(item.getValue(player))
-            object : BukkitRunnable() {
-                override fun run() {
-                    val location = player.location
-                    loc.y += 2.5
-                    display.teleport(location)
-                }
-            }.runTaskTimer(Main.getPlugin(), 0, 1L)
-        }*/
+        player.sendEquipmentChange(player, EquipmentSlot.HEAD, ItemStack(items[player]!!))
     }
 
     fun getItem(player: Player): Material? {
-        return item[player]
+        return items[player]
     }
 
     fun getBossbar(player: Player): BossBar {
-        if (item[player] == null) {
-            newItem(player)
-        }
-        val item = ItemStack(item[player]!!)
-        return BossBar.bossBar(Component.text("Current Item: ", NamedTextColor.YELLOW).append(item.displayName()), 1f, BossBar.Color.YELLOW, BossBar.Overlay.PROGRESS)
+        val item = ItemStack(items[player]!!)
+        return BossBar.bossBar(Component.text("Current Item: ", NamedTextColor.DARK_PURPLE).append(item.displayName().color(NamedTextColor.LIGHT_PURPLE)), 1f, BossBar.Color.PURPLE, BossBar.Overlay.PROGRESS)
     }
 
     fun sendBossbar(player: Player) {
+        val bossbars = Bukkit.getBossBars()
+        for (bossbar in bossbars) {
+            bossbar.removePlayer(player)
+        }
         player.showBossBar(getBossbar(player))
     }
 
-    /*fun collectedItem(mainHand: MainHand, player: Player) {
-        var main =
-        if (item !is )
-    }*/
-
     @EventHandler
-    fun itemCollected(event: InventoryPickupItemEvent, player: Player) {
-        if (event.item == item) {
+    fun itemCollected(event: EntityPickupItemEvent) {
+        if (event.entity !is Player) return
+        if (!Utilities().isChallengeEnabled(ChallengeManager.ALL_ITEMS, Main.getPlugin())) return
+        val player = event.entity as Player
+        if (items[player] == null) {
+            newItem(player)
+        } else {
+            if (event.item.itemStack.type != items[player]) return
             newItem(player)
         }
     }
-
 }
